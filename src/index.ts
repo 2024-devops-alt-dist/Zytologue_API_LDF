05c8a9300e0fd2449cde57bd74ad7a4966677775
LuchoDF <luchodellafelice@hotmail.com>
2025-01-17 11:34:47 +0100

    feat(cors) add cors for FE communication

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,18 @@ import routes from './routes/route';
 import swaggerUI from 'swagger-ui-express';
 import swaggerSpec from './swaggerConfig';
 export const APP_PORT = 3000;
+import cors from 'cors';
 
 dotenv.config(); 
 
 const app = express();
 
 //'use' method: to register routes or middlewares(function that runs during the life cycle of an http request)
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Frontend URL
+  })
+);
 app.use(express.json())
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use(routes) //all the routes in "routes" are now available in the server
