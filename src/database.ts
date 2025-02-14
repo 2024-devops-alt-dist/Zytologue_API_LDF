07fe902629e5710df32145ae480196b122902f8d
LuchoDF <luchodellafelice@hotmail.com>
2025-02-14 16:32:58 +0100

    fix database

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,22 @@
-import { Client } from "pg";
+import { Client } from 'pg';
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
 
 export const client = new Client({
-    user: process.env.DATABASE_USER as string ,
-    host: process.env.HOST as string,
-    database: process.env.DATABASE_NAME as string,
-    password: process.env.DATABASE_PASSWORD as string,
-    port: process.env.PORT as unknown as number,
+  user: process.env.DATABASE_USER as string,
+  host: process.env.HOST as string,
+  database: process.env.DATABASE_NAME as string,
+  password: process.env.DATABASE_PASSWORD as string,
+  port: process.env.DATABASE_PORT as unknown as number,
 });
 
 //connection to database
 
 export async function connectToDatabase() {
-    try {
-        await client.connect();
-        console.log("Successfully connected to the zytologue_API database!");
-    } catch (error) {
-        console.log("Error connecting to the zytologue_API database",error); 
-    }
+  try {
+    await client.connect();
+    console.log('Successfully connected to the zytologue_API database!');
+  } catch (error) {
+    console.log('Error connecting to the zytologue_API database', error);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,22 @@ import swaggerSpec from './swaggerConfig';
 export const APP_PORT = 3000;
 import cors from 'cors';
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 
 //'use' method: to register routes or middlewares(function that runs during the life cycle of an http request)
 app.use(
   cors({
-    origin: 'http://localhost:5173', // Frontend URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Frontend URL
   })
 );
-app.use(express.json())
+app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-app.use(routes) //all the routes in "routes" are now available in the server
+app.use(routes); //all the routes in "routes" are now available in the server
 
 app.listen(APP_PORT, () => {
-    console.log('Server running in http://localhost:3000');
-  });
-
+  console.log('Server running in http://localhost:3000');
+});
 
 connectToDatabase();
-
